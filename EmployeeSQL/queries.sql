6294da4e6010d7a39db770393e27c110717fa9e1
maddymcraven <maddymcraven@gmail.com>
2020-07-15 00:01:49 -0500

    commit

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -16,4 +16,25 @@ from employees as e, departments as d, dept_emp as de
 where e.emp_no = de.emp_no 
 and de.dept_no = d.dept_no
 
+select first_name, last_name, sex
+from employees
+where first_name = 'Hercules'
+AND left(last_name,1) = 'B'
 
+select e.emp_no, e.first_name, e.last_name, de.dept_no, d.dept_name
+from employees as e, departments as d, dept_emp as de
+where e.emp_no = de.emp_no 
+and de.dept_no = d.dept_no
+and d.dept_name = 'Sales'
+
+
+select e.emp_no, e.first_name, e.last_name, de.dept_no, d.dept_name
+from employees as e, departments as d, dept_emp as de
+where e.emp_no = de.emp_no 
+and de.dept_no = d.dept_no
+and (d.dept_name='Sales' or d.dept_name='Development')
+
+select count(emp_no), last_name
+from employees
+group by last_name
+order by count
